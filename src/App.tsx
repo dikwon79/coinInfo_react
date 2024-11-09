e4aac2453e374edfac957ca6dd9c31dfe757dae7
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-11-09 12:08:29 +0900

    optional props

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="yello" bgColor="teal" />
+      <Circle text="i'm here" bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
