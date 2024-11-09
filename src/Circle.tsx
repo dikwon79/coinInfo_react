0d2fee78bbf3f0db3238604cd29835746c8b38fd
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-11-10 06:26:53 +0900

    State

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -19,6 +20,7 @@ interface CircleProps {
   text?: string;
 }
 function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  const [counter, setCounter] = useState();
   return (
     <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
       {text}
