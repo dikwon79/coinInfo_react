353b138de88d275f66a35e46f48930222b0284d7
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-11-10 14:46:44 +0900

    finish typescript?

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "wihte",
+  textColor: "black",
+  btnColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "teal",
+};
