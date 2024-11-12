8c25e9337a926d6ecffdd347132494ba409afda1
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-11-12 10:38:45 +0900

    setting coing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import styled from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Btn = styled.button`
-  background-color: ${(props) => props.theme.bgColor};
-`;
 function App() {
-  return (
-    <Container>
-      <H1>protected</H1>
-      <Btn />
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./theme";
+import { Theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={Theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
