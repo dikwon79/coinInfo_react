d819691f03d08c3ee57096662478ca21c0f493e6
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-11-13 13:55:59 +0900

    state

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,46 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 interface RouteParams {
   coinId: string;
 }
-function Coin() {
-  const { coinId } = useParams<RouteParams>();
-  console.log(coinId);
 
-  return <h1>Coin: {coinId}</h1>;
+interface RouteState {
+  name: string;
+}
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  console.log(state.name);
+  return (
+    <Container>
+      <Header>
+        <Title>코인{state.name}</Title>
+      </Header>
+
+      {loading ? <Loader>Loading ...</Loader> : null}
+    </Container>
+  );
 }
 export default Coin;
